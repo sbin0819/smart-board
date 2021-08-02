60a1be30cf8dbcaea045dba19163edd35580f4a1
sbin0819 <sbinha123@gmail.com>
2021-08-02 10:18:30 +0900

    add: card detail init

--- front/src/components/Card.tsx
+++ front/src/components/Card.tsx
@@ -1,10 +1,47 @@
+import { useState } from 'react';
+
 import { ICard } from '../types/card';
 
+import { EditOutlined } from '@ant-design/icons';
+import styled from 'styled-components';
+
+import CardDetail from './CardDetail';
+
+const Container = styled.div`
+  background: #fff;
+  height: 32px;
+  border: 1px solid #d9d9d9;
+  margin-bottom: 4px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 12px;
+  cursor: pointer;
+`;
+
 function Card({ data }: { data: ICard[] }) {
+  const [detailInfo, setDetailInfo] = useState({});
+  const [isOpen, setIsOpen] = useState(false);
+  const onOpenCardDetail = () => setIsOpen(true);
+
+  const onCloseCardDetail = () => setIsOpen(false);
+
   return (
     <>
+      {isOpen && <CardDetail data={detailInfo} onClose={onCloseCardDetail} />}
       {data[0].card.map((d, idx) => (
-        <div key={idx}>{d.title}</div>
+        <>
+          <Container
+            key={idx}
+            onClick={() => {
+              setDetailInfo({ ...d });
+              onOpenCardDetail();
+            }}
+          >
+            <div>{d.title}</div>
+            <EditOutlined />
+          </Container>
+        </>
       ))}
     </>
   );
