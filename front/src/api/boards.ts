e1e886c0cad39e08bf8d6ee33d0db06605ebc2e3
sbin0819 <sbinha123@gmail.com>
2021-08-13 12:18:51 +0900

    add: post board test

--- front/src/api/boards.ts
+++ front/src/api/boards.ts
@@ -5,3 +5,9 @@ export const getBoards = async () => {
   const response = await axios.get(NEST_URL + '/boards');
   return response;
 };
+
+export const postBoard = async (title: string) => {
+  const response = await axios.post(NEST_URL + '/boards', { title });
+  console.log(response);
+  return response;
+};
--- front/src/pages/sub.tsx
+++ front/src/pages/sub.tsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom';
 import { useStore } from '../context';
 import { useEffect } from 'react';
 
-import { getBoards } from '../api/boards';
+import { getBoards, postBoard } from '../api/boards';
 
 function Sub() {
   const [{ memo }] = useStore();
@@ -14,6 +14,14 @@ function Sub() {
     <div>
       <h1>SUB PAGE</h1>
       <Link to="/">HOME</Link>
+      <div>
+        <button
+          style={{ height: 40, fontSize: 30, padding: '0 20px' }}
+          onClick={() => postBoard('new board')}
+        >
+          create a board
+        </button>
+      </div>
     </div>
   );
 }
